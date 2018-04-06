0703fcdfae3ee91b7ccca28693890465dbd8c38a
BunnygotFangs <kimberly@thehilts.com>
2018-04-06 15:45:31 -0500

    uploading of homework files for submission

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+//Define the needed variables for the game
+
+$(document).ready(function() 
+
+{
+var lightSide;
+var lightSideHealth;
+var lightSideSabre;
+var darkSide;
+var darkSideHealth;
+var darkSideSabre;
+var attack
+var win
+var loss
+
+// Provide instructions for game play
+
+// LightSide Character is selected for battle
+
+// When first character is selected fade character out in character section
+// Fade in selected character to Battle area for light side
+// Generate a random number for Lightside player health between (100 - 250)
+// Generate a random lightsabre number (10 - 25)
+
+// Darkside Character is selected for battle
+
+// When first character is selected fade character out in character section
+// Fade in selected character to Battle area for Dark side
+// Generate a random number for Darkside player health 100 - 250
+// Generate a random lightsabre number (10 - 25)
+
+// Attack button is clicked to begin battle
+// Randomly pick Light or Dark side for battle strike
+// If player 1 (light side); reduce health of Player 2 (dark side)
+// If player 2 (dark side); reduce health of Player 1 (light side)
+
+// Game over
+// When a player reach 0 health game is over and winner is declared
+// Looser image is faded away
+// Next click will rest the game for new player selection
+
+// Game over
+
+
+})

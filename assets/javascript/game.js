f5b1bd2fbaab1a21d1abac853c506709971244a1
BunnygotFangs <kimberly@thehilts.com>
2018-04-06 22:08:55 -0500

    finally got health to reflect in game board; added winner or looser; still working out hiccup to attack

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,14 @@
 $(document).ready(function() 
 
 {
-var lightSide;
-var lightSideHealth = (Math.floor(Math.random() * 250));
-var lightSideSabre = 1;
-var darkSide;
-var darkSideHealth = (Math.floor(Math.random() * 250));
-var darkSideSabre = 2;
+var lightSide= 1;
+var lightSideHealth;
+var lightSideSaber;
+var darkSide = 2;
+var darkSideHealth;
+var darkSideSaber; 
 var attack;
+var saber;
 var win;
 var loss;
 
@@ -25,12 +26,16 @@ var loss;
     $(this).hide("#one");
 // Fade in selected character to Battle area for light side
     $("#batone").append('<img src="assets/images/Yoda.jpeg" width="150" height="150" />' );
+    lightSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-light").text(lightSideHealth);
 // Generate a random number for Lightside player health between (100 - 250)
 
 // Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
-    if ("#batone" === true)
+    if("#batone" == true)
     {
     $("#battwo").append('<img src="assets/images/Yoda.jpeg" width="150" height="150"/>');
+    darkSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-dark").text(darkSideHealth);
     //elseif (!"#batwo" === false), alert("You Are Ready for Battle. Click 'OK' to Continue.")
     }
 }); 
@@ -45,10 +50,13 @@ var loss;
 // Fade in selected character to Battle area for light side
     $("#batone").append('<img src="assets/images/Han Solo.jpeg" width="150" height="150" />' );
 // Generate a random number for Lightside player health between (100 - 250)
-   
+    lightSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-light").text(lightSideHealth);
 // Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
-    if ("#batone" === true)
+    if ("#batone" == true)
     $("#battwo").append('<img src="assets/images/Han Solo.jpeg" width="150" height="150"/>');
+    darkSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-dark").text(darkSideHealth);
     //else alert("You Are Ready for Battle. Click 'OK' to Continue.");
 }); 
 
@@ -60,10 +68,13 @@ var loss;
 // Fade in selected character to Battle area for light side
     $("#batone").append('<img src="assets/images/ewok.jpeg" width="150" height="150" />' );
 // Generate a random number for Lightside player health between (100 - 250)
-   
+    lightSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-light").text(lightSideHealth);
 // Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
-    if ("#batone" === true)
+    if ("#batone" == true)
     $("#battwo").append('<img src="assets/images/ewok.jpeg" width="150" height="150"/>');
+    darkSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-dark").text(darkSideHealth);
     //else  alert("You Are Ready for Battle. Click 'OK' to Continue.");
 }); 
 
@@ -75,10 +86,13 @@ var loss;
 // Fade in selected character to Battle area for light side
     $("#batone").append('<img src="assets/images/Sidious.jpeg" width="150" height="150" />' );
 // Generate a random number for Lightside player health between (100 - 250)
-   
+    lightSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-light").text(lightSideHealth);
 // Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
-    if ("#batone" === true)
+    if ("#batone" == true)
     $("#battwo").append('<img src="assets/images/Sidious.jpeg" width="150" height="150"/>');
+    darkSideHealth = (Math.floor(Math.random() * 250));
+    $(".badge-dark").text(darkSideHealth);
     //else  alert("You Are Ready for Battle. Click 'OK' to Continue.");
 }); 
 // Attack button is clicked to begin battle
@@ -89,16 +103,35 @@ $(document).on('click', '.btn-danger', function()
     console.log('is the button working')
 // Randomly pick Light or Dark side for battle strike
 attack = (Math.floor(Math.random() * 3));
+saber = (Math.floor(Math.random() * 25));
+var dSH2;
+var lSH2;
 
 console.log(attack);
+console.log(saber);
 
+        if (attack = lightSide)
+    { 
+        dSH2 = (darkSideHealth - saber);
+        console.log(dSH2);
+        $('.badge-dark').text(dSH2);
+    }
+      else
+    {    
+        lSH2 = (lightSideHealth - saber);
+        console.log(lSH2);
+        $('.badge-light').text(lSH2);
+    }
+        
+    if (dSH2 <= 0)
+    {    
+        $('#wins').append("<h2> YOU HAVE DEFEATED YOUR ENEMY FORCES! </h2>");
+    }
+    else
+    {
+        $('#losses').append("<h2> YOU BEEN DESTROYED BY YOUR ENEMY! </h2>");
+    }
 }
-); 
-
-});
-
-
-
 // Generate a random number for Lightside player health between (100 - 250)
 // Generate a random lightsabre number (10 - 25)
 
@@ -120,3 +153,4 @@ console.log(attack);
 
 // Game over
 // Functions for the game
+)});

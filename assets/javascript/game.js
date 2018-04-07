d81597dba2a7d6969f9e9460b59fa9da24f35457
BunnygotFangs <kimberly@thehilts.com>
2018-04-06 19:04:12 -0500

    saving updates from thursdays JS updating repository with JS from thursdays work moving players and trying to get attack button to work before hitting wall of whats next

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,115 @@
 //Define the needed variables for the game
-
+// console.log ('is html reading me')
 $(document).ready(function() 
 
 {
 var lightSide;
-var lightSideHealth;
-var lightSideSabre;
+var lightSideHealth = (Math.floor(Math.random() * 250));
+var lightSideSabre = 1;
 var darkSide;
-var darkSideHealth;
-var darkSideSabre;
-var attack
-var win
-var loss
+var darkSideHealth = (Math.floor(Math.random() * 250));
+var darkSideSabre = 2;
+var attack;
+var win;
+var loss;
 
 // Provide instructions for game play
 
 // LightSide Character is selected for battle
 
+
+    $(document).on('click', '#one', function()
+{
 // When first character is selected fade character out in character section
+    console.log('yo yoda');
+    $(this).hide("#one");
 // Fade in selected character to Battle area for light side
+    $("#batone").append('<img src="assets/images/Yoda.jpeg" width="150" height="150" />' );
+// Generate a random number for Lightside player health between (100 - 250)
+
+// Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
+    if ("#batone" === true)
+    {
+    $("#battwo").append('<img src="assets/images/Yoda.jpeg" width="150" height="150"/>');
+    //elseif (!"#batwo" === false), alert("You Are Ready for Battle. Click 'OK' to Continue.")
+    }
+}); 
+
+
+
+    $(document).on('click', '#two', function()
+{
+// When first character is selected fade character out in character section
+    console.log('Whatever solo');
+    $(this).hide("#two");
+// Fade in selected character to Battle area for light side
+    $("#batone").append('<img src="assets/images/Han Solo.jpeg" width="150" height="150" />' );
+// Generate a random number for Lightside player health between (100 - 250)
+   
+// Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
+    if ("#batone" === true)
+    $("#battwo").append('<img src="assets/images/Han Solo.jpeg" width="150" height="150"/>');
+    //else alert("You Are Ready for Battle. Click 'OK' to Continue.");
+}); 
+
+    $(document).on('click', '#three', function()
+{
+// When first character is selected fade character out in character section
+    console.log('damn ewoks');
+    $(this).hide("#three");
+// Fade in selected character to Battle area for light side
+    $("#batone").append('<img src="assets/images/ewok.jpeg" width="150" height="150" />' );
+// Generate a random number for Lightside player health between (100 - 250)
+   
+// Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
+    if ("#batone" === true)
+    $("#battwo").append('<img src="assets/images/ewok.jpeg" width="150" height="150"/>');
+    //else  alert("You Are Ready for Battle. Click 'OK' to Continue.");
+}); 
+
+    $(document).on('click', '#four', function()
+{
+// When first character is selected fade character out in character section
+    console.log('evil evil man');
+    $(this).hide("#four");
+// Fade in selected character to Battle area for light side
+    $("#batone").append('<img src="assets/images/sidious.jpeg" width="150" height="150" />' );
+// Generate a random number for Lightside player health between (100 - 250)
+   
+// Fade in selected character to Battle area for Dark side if battle character 1 is populated (light side)
+    if ("#batone" === true)
+    $("#battwo").append('<img src="assets/images/sidious.jpeg" width="150" height="150"/>');
+    //else  alert("You Are Ready for Battle. Click 'OK' to Continue.");
+}); 
+// Attack button is clicked to begin battle
+$(document).on('click', '.btn-danger', function()
+
+{
+// When first character is selected fade character out in character section
+    console.log('is the button working')
+// Randomly pick Light or Dark side for battle strike
+attack = (Math.floor(Math.random() * 3));
+
+console.log(attack);
+
+}
+); 
+
+});
+
+
+
 // Generate a random number for Lightside player health between (100 - 250)
 // Generate a random lightsabre number (10 - 25)
 
 // Darkside Character is selected for battle
 
-// When first character is selected fade character out in character section
-// Fade in selected character to Battle area for Dark side
+
 // Generate a random number for Darkside player health 100 - 250
 // Generate a random lightsabre number (10 - 25)
 
-// Attack button is clicked to begin battle
-// Randomly pick Light or Dark side for battle strike
+
+
 // If player 1 (light side); reduce health of Player 2 (dark side)
 // If player 2 (dark side); reduce health of Player 1 (light side)
 
@@ -40,6 +119,4 @@ var loss
 // Next click will rest the game for new player selection
 
 // Game over
-
-
-})
+// Functions for the game
